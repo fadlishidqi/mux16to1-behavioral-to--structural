2d6cad307926a22023f2d7f5946a69b5c0c06096
fadlishidqi <118976398+fadlishidqi@users.noreply.github.com>
2024-09-19 15:53:55 +0700

    Update mux2to1gate.v

---
+++ mux2to1gate.v
@@ -0,0 +1,12 @@
+// Fadli Shidqi Firdaus
+// 21120122140166
+// SDL C
+
+module mux2to1gate (input a, input b, input sel, output y);
+    wire sel_not, a_and, b_and;
+
+    not (sel_not, sel);
+    and (a_and, a, sel_not);
+    and (b_and, b, sel);
+    or (y, a_and, b_and);
+endmodule
